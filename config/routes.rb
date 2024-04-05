1bad8d21ad2f634f7a4ae77fce479e9fa401d93b
Anna Patricia Entrada <triciaentrada@gmail.com>
2024-04-05 12:04:32 +0800

    rails intsllation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#home"
+
+  get '/home', to: 'welcome#home'
+
+  get '/about', to: 'welcome#about'
+
+  get '/contact', to: 'welcome#contact'
+
 end
