a8a0b66cd09e504f7afe73955b58baf8c119aaec
Anna Patricia Entrada <triciaentrada@gmail.com>
2024-04-12 13:34:23 +0800

    add: active record for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_05_075011) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_12_033635) do
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.bigint "post_id", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_05_075011) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   add_foreign_key "comments", "posts"
