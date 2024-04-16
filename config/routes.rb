f87152b329b9e5538a996067bcd82717d1e2e7fa
Anna Patricia Entrada <triciaentrada@gmail.com>
2024-04-16 14:59:29 +0800

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  devise_for :users
   root "welcome#home"
 
-  get '/home', to: 'welcome#home'
+  get 'home', to: 'welcome#home'
 
-  get '/about', to: 'welcome#about'
+  get 'about', to: 'welcome#about'
 
-  get '/contact', to: 'welcome#contact'
+  get 'contact', to: 'welcome#contact'
 
   resources :posts do
     resources :comments, except: :show
